198fe3aac0bf7319f947b42d8c7482924f32eecc
keshav33 <38713053+keshav33@users.noreply.github.com>
2020-09-15 18:18:59 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const cors = require('cors')
 
-mongoose.connect('mongodb://localhost/assignment_db', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/assignment_db', { useNewUrlParser: true });
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
@@ -46,9 +46,43 @@ app.post('/add-value', (req, res) => {
 })
 
 app.get('/list-of-values', (req, res) => {
-    Value.find({}, (err,result)=>{
+    Value.find({}, (err, result) => {
         console.log(result)
         res.json(result)
+    })
+})
+
+app.post('/list-of-value-delete', (req, res) => {
+    let _id = req.body._id
+    Value.deleteOne({ _id: _id }, (err, result) => {
+        res.json(result)
+    })
+})
+
+app.post('/list-of-value-edit', (req, res) => {
+    let _id = req.body._id
+    let name = req.body.name
+    let email = req.body.email
+    let phone = req.body.phone
+    let salary = req.body.salary
+
+    Value.findOne({ _id }, (err, result)=>{
+        value = result
+        if(name !== ''){
+            value.name = name
+        }
+        if(email !== ''){
+            value.email = email
+        }
+        if(phone !== ''){
+            value.phone = phone
+        }
+        if(salary !== ''){
+            value.salary = salary
+        }
+        Value.findByIdAndUpdate({_id},value, (err, result)=>{
+            res.json(result)
+        })
     })
 })
 
